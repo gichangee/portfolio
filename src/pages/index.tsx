d6887f6f27b5b31c473c9f67e89d466641f43a29
gichangee <parkgc0504@naver.com>
2024-07-22 09:04:38 +0900

    add info

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,12 +32,12 @@ const Home: NextPage<DataProps> = ({
       <ResumeTitle resumeTitle={resumeTitle} />
       <Layout>
         <Information information={information} />
-        <WorkExperience workExperience={workExperience} />
+         {/* <WorkExperience workExperience={workExperience} />*/}
         <Project project={project} />
         <Activity activity={activity} />
         <Education education={education} />
         <Certificate certificate={certificate} />
-        <Award award={award} />
+         {/* <Award award={award} /> */}
       </Layout>
       <Footer contact={information.contact} name={information.name} />
     </>
